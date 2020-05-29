fef0b14a440a24e8053af355f028ad758e109bb5
joshuamsje <49621706+joshuamsje@users.noreply.github.com>
2020-05-28 20:35:05 -0400

    Create DNAFlyweightNode.java

---
+++ DNAFlyweightNode.java
@@ -0,0 +1,22 @@
+package dnaTree;
+/**
+ * It marks on the internal node and marks branches with no data.
+ * There's only one Flyweight node.
+ * 
+ * @author Kurt Karpin (kkarp9)
+ * @author Joshua Mathew (josh827)
+ *
+ * @version 2020.3.29
+ */
+public class DNAFlyweightNode extends DNANode {
+    
+    /**
+     * Creates Flyweight Node that does
+     * nothing.
+     */
+    public DNAFlyweightNode() {
+       //empty
+    }
+
+    
+}
